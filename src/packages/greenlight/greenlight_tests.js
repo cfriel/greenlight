6ca56fe39ffc190fe74a3f3f13858bb48e3459ce
Chris Friel <cfriel@gmail.com>
2013-08-10 01:43:38 -0400

    updating create page

---
+++ src/packages/greenlight/greenlight_tests.js
@@ -0,0 +1,32 @@
+// Greenlight.Helpers.load_item(
+//     'mongodb://127.0.0.1:27017/',
+//     'examples',
+//     'rounds',
+//     ObjectID('52044617e71700ea13012575'));
+
+// Greenlight.Helpers.load_data(
+//     'mongodb://127.0.0.1:27017/',
+//     'examples',
+//     'rounds',
+//     {},
+//     0,
+//     100);
+
+// Greenlight.Helpers.load_databases(
+//     'mongodb://127.0.0.1:27017/'
+// );
+
+// var schema = 
+//     Greenlight.Helpers.analyze_schema(
+// 	'mongodb://127.0.0.1:27017/',
+// 	'examples',
+// 	'rounds'
+//     );
+
+// Greenlight.log(JSON.stringify(schema), []);
+
+// Greenlight.Helpers.load_schema(
+//     'mongodb://127.0.0.1:27017/',
+//     'examples',
+//     'rounds'
+// );
