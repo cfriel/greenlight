5509dbaa66f39c1645e58778f43dfd54a883d4c6
Chris Friel <cfriel@gmail.com>
2013-09-29 21:25:32 -0400

    transforms

--- src/packages/greenlight/client/greenlight.transform.js
+++ src/packages/greenlight/client/greenlight.transform.js
@@ -19,8 +19,18 @@ greenlight.prototype.Transform.prototype = new Greenlight.Entity();
 greenlight.prototype.Transform.prototype.constructor = greenlight.prototype.Transform;
 Greenlight.Transforms.entity = greenlight.prototype.Transform;
 
+greenlight.prototype.Transform.prototype.save = function()
+{
+    var self = this;
+
+    Greenlight.Transforms.insert({ owner: Meteor.userId(), 
+				  name: self.name,
+				  configuration: self.configuration});
+};
+
 greenlight.prototype.Transform.prototype.create = function()
 {
+
 };
 
 greenlight.prototype.Transform.prototype.register = function()
